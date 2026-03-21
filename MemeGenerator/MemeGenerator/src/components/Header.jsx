00bdcdb2568938a2b68eb2060f2234b7219c2b72
Digant Bhanwriya <2021pcs2031@iitjammu.ac.in>
2026-03-21 22:56:45 +0530

    React with side effects completed, now moving to capstone project

---
+++ MemeGenerator/MemeGenerator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return  (
+        <header className="meme-header">
+            <div className="header-content">
+                <span className="header-icon">😂</span>
+                <h1>Meme Generator</h1>
+            </div>
+        </header>
+    );
+}
--- React/chefClaude/components/IngredientList.jsx
+++ React/chefClaude/components/IngredientList.jsx
@@ -1,11 +1,11 @@
-export default function IngredientList({ ingredientList , onGetRecipe}) {
+export default function IngredientList({ ref, ingredientList , onGetRecipe}) {
    const ingredientElements = ingredientList.map( (ingredient,index) => <li key={index} className="ingredient-item">{ingredient}</li>);
    return (
         <section className="ingredients-section">
                 <h2 className='ingredient-list-label'>Ingredients List : </h2>
                 <ul className="ingredient-list">{ingredientElements}</ul>
                 {ingredientList.length > 3 && <div className='ingredient-actions'>
-                    <div>
+                    <div ref={ref} className="recipe-prompt">
                         <h3>Ready for a recipe?</h3>
                         <p> Generate a recipe based on your ingredients!</p>
                     </div> 
